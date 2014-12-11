95fa7444fbe31e4233f98507681515671ebc0a03
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 12:19:27 -0500

    add benching

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-inside = require('turf-inside')
+inside = require('turf-inside');
 
 module.exports = function(points, polygons, field, outField){
   points.features.forEach(function(pt){
     if(!pt.properties){
-      pt.properties = {}
+      pt.properties = {};
     }
     polygons.features.forEach(function(poly){
       if(!pt.properties[outField]){
-        var isInside = inside(pt, poly)
+        var isInside = inside(pt, poly);
         if(isInside){
-          pt.properties[outField] = poly.properties[field]
+          pt.properties[outField] = poly.properties[field];
         }
         else{
-          pt.properties[outField] = null
+          pt.properties[outField] = null;
         }
       }
-    })
+    });
   })
-  return points
+  return points;
 }
