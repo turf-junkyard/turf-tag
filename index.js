a0409732bd20707de026e2d4358aa4d8f66af021
Tom MacWright <tom@macwright.org>
2015-01-16 10:03:19 -0500

    Fix logic & tests

--- index.js
+++ index.js
@@ -12,53 +12,43 @@ var inside = require('turf-inside');
  * @param {string} containingPolyId
  * @return {LineString} output
  * @example
-var bbox = [0, 0, 50, 50];
-
-// create a triangular grid of polygons
-var triangleGrid = turf.tin(turf.grid(bbox, 10));
-
-triangleGrid.features.forEach(function(f) {
-  f.properties.fill = '#' +
-    (~~(Math.random() * 16)).toString(16) +
-    (~~(Math.random() * 16)).toString(16) +
-    (~~(Math.random() * 16)).toString(16);
-  f.properties.stroke = 0;
-  f.properties['fill-opacity'] = 1;
-});
-
-var randomPoints = turf.random('point', 30, {
-  bbox: bbox
-});
-
-var both = turf.featurecollection(
-  triangleGrid.features.concat(randomPoints.features));
-
-//=both
-
-var tagged = turf.tag(randomPoints, triangleGrid,
-                      'fill', 'marker-color');
-
-//=tagged
+ * var bbox = [0, 0, 50, 50];
+ * // create a triangular grid of polygons
+ * var triangleGrid = turf.tin(turf.grid(bbox, 10));
+ * triangleGrid.features.forEach(function(f) {
+ *   f.properties.fill = '#' +
+ *     (~~(Math.random() * 16)).toString(16) +
+ *     (~~(Math.random() * 16)).toString(16) +
+ *     (~~(Math.random() * 16)).toString(16);
+ *   f.properties.stroke = 0;
+ *   f.properties['fill-opacity'] = 1;
+ * });
+ * var randomPoints = turf.random('point', 30, {
+ *   bbox: bbox
+ * });
+ * var both = turf.featurecollection(
+ *   triangleGrid.features.concat(randomPoints.features));
+ * //=both
+ * var tagged = turf.tag(randomPoints, triangleGrid,
+ *                       'fill', 'marker-color');
+ * //=tagged
  */
 module.exports = function(points, polygons, field, outField){
   // prevent mutations
   points = JSON.parse(JSON.stringify(points));
   polygons = JSON.parse(JSON.stringify(polygons));
-  points.features.forEach(function(pt){
-    if(!pt.properties){
+  points.features.forEach(function(pt) {
+    if (!pt.properties) {
       pt.properties = {};
     }
-    polygons.features.forEach(function(poly){
-      if(pt.properties[outField] === undefined){
+    polygons.features.forEach(function(poly) {
+      if (pt.properties[outField] === undefined) {
         var isInside = inside(pt, poly);
-        if(isInside){
+        if (isInside) {
           pt.properties[outField] = poly.properties[field];
-        }
-        else{
-          pt.properties[outField] = null;
         }
       }
     });
-  })
+  });
   return points;
-}
+};
